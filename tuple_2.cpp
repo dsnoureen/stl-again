3625e194189186ae1cc2c7e7a28185ef8825c520
Dewan Sadia Noureen <dewansadianoureen02@gmail.com>
2025-12-07 19:48:56 +0600

    tuple loop

---
+++ tuple_2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    tuple<string, int, string> persons[n];
+    string name;
+    int age;
+    string dept;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> get<0>(persons[i]) >> get<1>(persons[i]) >> get<2>(persons[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << get<0>(persons[i]) << " " << get<1>(persons[i]) << " " << get<2>(persons[i])<<endl;
+    }
+    return 0;
+}
