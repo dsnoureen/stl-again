f3d5be4f16039e2cc2d766fa5e5d2038b112bfd6
Dewan Sadia Noureen <dewansadianoureen02@gmail.com>
2025-12-07 17:01:04 +0600

    pair

---
+++ pair.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    pair<string, int> person;
+    cin >> person.first >> person.second;
+    string name;
+    int age;
+    person.first=name;
+    person.second=age;
+    cout << person.first << " " << person.second << endl;
+    return 0;
+}
