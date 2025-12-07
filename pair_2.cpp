a88776659e8eccf3981392133ddc1408d8dbd503
Dewan Sadia Noureen <dewansadianoureen02@gmail.com>
2025-12-07 19:53:11 +0600

    pair loop

--- pair_2.cpp
+++ pair_2.cpp
@@ -1,6 +1,17 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    
+int main()
+{
+    int n;
+    cin >> n;
+    pair<string, int> persons[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> persons[i].first >> persons[i].second;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << persons[i].first << " " << persons[i].second << endl;
+    }
     return 0;
 }
